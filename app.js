de02a11a28e08d6b539d097ca3207ab80de1a09d
Vitor Caetano <vitorkaetano343@gmail.com>
2026-04-14 16:10:59 -0300

    função add implementada

---
+++ app.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const path = './taskList.json'
+
+const command = process.argv[2];
+const args = process.argv.slice(3);
+
+
+function add(args){
+    if (!fs.existsSync(path)) {
+        const content = "[]"       
+        fs.writeFileSync(path, content, 'utf-8')
+        console.log(`Arquivo ${path} criado com sucesso!`);
+    }
+
+    let tasks = [];
+    
+    const dados = fs.readFileSync('taskList.json', 'utf-8')
+    tasks = JSON.parse(dados);
+    
+    const novoId = tasks.length > 0
+    ? Math.max(...tasks.map(t => t.id)) + 1
+    : 1; 
+    
+    const newTask = {
+        id: novoId,
+        description: args[0],
+        status: "todo",
+        createdAt: new Date().toLocaleString('pt-BR'),
+        updatedAt: new Date().toLocaleString('pt-BR')        
+    }
+
+    tasks.push(newTask)
+
+    fs.writeFileSync('taskList.json', JSON.stringify(tasks, null, 2));
+    
+    console.log("Tarefa adicionada com sucesso")
+}
+
+//function list(){
+
+//}
+
+
+const commands = {
+    add: add,
+    //list: list,
+};
+
+if (commands[command]) {
+    commands[command](args);
+} else {
+    console.log("Comando inválido");
+}
